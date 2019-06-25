bde42d2830c515a791cc44b4692b23c47fa239a0
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2019-06-25 04:10:02 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -3,11 +3,16 @@ def hopper
 	programmer_hash = 
  		{
         :grace_hopper => {
-          :known_for => "COBOL",
-        
-        
-      
-          :languages => ["C"]
+        :known_for => "COBOL",
+        :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+        :known_for => "Object Orientation",
+        :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+        :known_for => "Unix",
+        :languages => ["C"]
         }
      }
   programmer_hash[:grace_hopper]
