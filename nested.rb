d906c9b442f6f635cf3c5d6157502e1e5927e0f4
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2019-06-25 03:46:17 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -33,14 +33,7 @@ def dennis_ritchies_language
  		{
         :grace_hopper => {
           :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
+          
           :languages => ["C"]
         }
      }
