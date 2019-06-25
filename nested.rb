ec3668a46d1221717fbf764195bd936fdc4dca7f
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2019-06-25 04:11:10 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -25,9 +25,16 @@ def alan_kay_is_known_for
 	programmer_hash = 
  		{
         :grace_hopper => {
-          :known_for => "COBOL",
-          
-          :languages => ["C"]
+        :known_for => "COBOL",
+        :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+        :known_for => "Object Orientation",
+        :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+        :known_for => "Unix", 
+        :languages => ["C"]
         }
      }
      programmer_hash[:alan_kay][:known_for]
