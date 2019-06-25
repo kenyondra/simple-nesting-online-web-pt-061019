50a993822b045d0e5a8656dfb4a5860df1a4ce07
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2019-06-25 04:12:32 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -44,9 +44,16 @@ def dennis_ritchies_language
 	programmer_hash = 
  		{
         :grace_hopper => {
-          :known_for => "COBOL",
-          
-          :languages => ["C"]
+        :known_for => "COBOL",
+        :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+        :known_for => "Object Orientation",
+        :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+        :known_for => "Unix", 
+        :languages => ["C"]
         }
      }
      x = programmer_hash[:dennis_ritchie][:languages]
